2de820f9af256d1225e1e3e5a486ee27889ced08
Grouse16 <ryuseit16@gmail.com>
2023-11-07 17:58:07 +0900

    3Dモデル使用システムの設計
    クラス図を用いて3Dモデルをどのように使用するかの構想をたてた。
    叩き出しなので実装中にも更新を行う

---
+++ Project/Elina_Lead/Elina_Lead/C_3D_Model_System.h
@@ -0,0 +1,26 @@
+//☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆//
+// 詳細   ：3Dモデル用のシステム
+// 説明   ：3Dモデルの取得から描画、アニメーションの実行までを行う
+// 作成者 ：髙坂龍誠
+//☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆//
+
+
+// ☆ 多重インクルードガード ☆ //
+#ifndef D_INCLUDE_GUARD_C_3D_MODEL_SYSTEM_H_FILE
+#define D_INCLUDE_GUARD_C_3D_MODEL_SYSTEM_H_FILE
+
+
+// ☆ ネームスペース ☆ //
+
+// 3Dモデルを呼び出すための名前
+namespace ASSET::MODEL
+{
+	// ☆ クラス ☆ //
+
+	// 3Dモデル用のシステム
+	class C_3D_Model_System
+	{
+	};
+}
+
+#endif // !D_INCLUDE_GUARD_C_3D_MODEL_SYSTEM_H_FILE
