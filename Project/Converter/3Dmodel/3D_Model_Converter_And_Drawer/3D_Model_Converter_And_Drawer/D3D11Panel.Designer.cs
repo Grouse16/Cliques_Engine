044c905c19000f32ddc2539db9ba803773ac5223
Grouse16 <ryuseit16@gmail.com>
2023-11-07 23:11:40 +0900

    .NETによる3Dモデルコンバーターの制作を開始
    基本的なUIはある程度実装したので、コンバートとテスト表示のシステムを制作する

---
+++ Project/Converter/3Dmodel/3D_Model_Converter_And_Drawer/3D_Model_Converter_And_Drawer/D3D11Panel.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace SharpDXSample
+{
+    partial class D3D11Panel
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // D3D11Panel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "D3D11Panel";
+            this.Size = new System.Drawing.Size(526, 336);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Project/Converter/3Dmodel/3D_Model_Converter_And_Drawer/3D_Model_Converter_And_Drawer/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3D_Model_Converter_And_Drawer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // フォーム読み込み時
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = true;
+            textBox2.ReadOnly = true;
+            textBox3.ReadOnly = true;
+            textBox4.ReadOnly = true;
+            textBox5.ReadOnly = true;
+            textBox6.ReadOnly = true;
+            textBox7.ReadOnly = true;
+            textBox8.ReadOnly = true;
+            textBox9.ReadOnly = true;
+            textBox10.ReadOnly = true;
+            textBox11.ReadOnly = true;
+            textBox12.ReadOnly = true;
+            textBox13.ReadOnly = true;
+            textBox14.ReadOnly = true;
+            textBox15.ReadOnly = true;
+            textBox16.ReadOnly = true;
+            textBox17.ReadOnly = true;
+            TB_after_convert_path.ReadOnly = true;
+            TB_before_convert_path.ReadOnly = true;
+
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+        }
+
+
+        // ドラッグがカーソル上にあるとき
+        private void B_Drag_Over_File_Checker(object sender, DragEventArgs e)
+        {
+            // ファイルドロップ時はカーソルをコピーに変更
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+
+            // それ以外の時はなにもしない
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+
+    }
+}
