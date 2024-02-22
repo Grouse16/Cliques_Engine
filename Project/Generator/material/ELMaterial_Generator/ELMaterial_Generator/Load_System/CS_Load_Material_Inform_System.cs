4fbbb94805019803d4107a9afaf0887c9e631c12
Grouse16 <ryuseit16@gmail.com>
2024-02-22 13:21:44 +0900

    マテリアルの質感情報ファイル(.elmatinform)をロードするシステムを実装
    ◆実装内容
     ・ マテリアル生成アプリにドラッグアンドドロップで受け取ったマテリアル質感情報ファイルから、マテリアル質感データをロードするシステムを実装
        - ロードしたデータをまとめるためのマテリアル質感用の構造体を追加

--- Project/Generator/material/ELMaterial_Generator/ELMaterial_Generator/Load_System/CS_Load_Material_Inform_System.cs
+++ Project/Generator/material/ELMaterial_Generator/ELMaterial_Generator/Load_System/CS_Load_Material_Inform_System.cs
@@ -11,7 +11,7 @@ namespace ELMaterial_Generator.Load_System
 {
 	// ☆ 構造体 ☆ //
 
-	// マテリアル設定のデータ
+	// マテリアル設定のデータの構造体
 	public struct S_Load_Material_Inform
 	{
 		// ☆ 変数宣言 ☆ //
@@ -40,16 +40,18 @@ namespace ELMaterial_Generator.Load_System
 		private S_RGBA_Data transparent_data;	// トランスペアレントデータ
 
 
-
 		// ☆ プロパティ ☆ //
 
 		// マテリアル名
 		public string mp_material_name
 		{
+			// ゲッタ
 			get
 			{
 				return material_name;
 			}
+
+			// セッタ
 			set
 			{
 				material_name = value;
@@ -59,10 +61,13 @@ namespace ELMaterial_Generator.Load_System
 		// シェーダー名
 		public string mp_shader_name
 		{
+			// ゲッタ
 			get
 			{
 				return shader_name;
 			}
+
+			// セッタ
 			set
 			{
 				shader_name = value;
@@ -72,10 +77,13 @@ namespace ELMaterial_Generator.Load_System
 		// ブレンド設定数
 		public int mp_blend_sum
 		{
+			// ゲッタ
 			get
 			{
 				return blend_sum;
 			}
+
+			// セッタ
 			set
 			{
 				blend_sum = value;
@@ -85,10 +93,13 @@ namespace ELMaterial_Generator.Load_System
 		// 書き込み設定リスト
 		public List<E_WRITE_SETTING> mp_write_setting_list
 		{
+			// ゲッタ
 			get
 			{
 				return write_setting_list;
 			}
+
+			// セッタ
 			set
 			{
 				write_setting_list = value;
@@ -98,10 +109,13 @@ namespace ELMaterial_Generator.Load_System
 		// 深度モード
 		public E_DEPTH_MODE mp_depth_mode
 		{
+			// ゲッタ
 			get
 			{
 				return depth_mode;
 			}
+
+			// セッタ
 			set
 			{
 				depth_mode = value;
@@ -111,10 +125,13 @@ namespace ELMaterial_Generator.Load_System
 		// 描画モード
 		public E_DRAW_MODE mp_draw_mode
 		{
+			// ゲッタ
 			get
 			{
 				return draw_mode;
 			}
+			
+			// セッタ
 			set
 			{
 				draw_mode = value;
@@ -124,10 +141,13 @@ namespace ELMaterial_Generator.Load_System
 		// カリングモード
 		public E_CULL_MODE mp_cull_mode
 		{
+			// ゲッタ
 			get
 			{
 				return cull_mode;
 			}
+
+			// セッタ
 			set
 			{
 				cull_mode = value;
@@ -137,10 +157,13 @@ namespace ELMaterial_Generator.Load_System
 		// アンチエイリアシングモード
 		public E_ANTI_ALIASING_MODE mp_anti_aliasing_mode
 		{
+			// ゲッタ
 			get
 			{
 				return anti_aliasing_mode;
 			}
+
+			// セッタ
 			set
 			{
 				anti_aliasing_mode = value;
@@ -150,10 +173,13 @@ namespace ELMaterial_Generator.Load_System
 		// サンプリングレベル
 		public int mp_sampling_level
 		{
+			// ゲッタ
 			get
 			{
 				return sampling_level;
 			}
+
+			// セッタ
 			set
 			{
 				sampling_level = value;
@@ -163,80 +189,98 @@ namespace ELMaterial_Generator.Load_System
 		// アンビエントデータ
 		public S_RGBA_Data mp_ambient_data
 		{
-            get
+			// ゲッタ
+			get
 			{
-                return ambient_data;
-            }
-            set
+				return ambient_data;
+			}
+
+			// セッタ
+			set
 			{
-                ambient_data = value;
-            }
-        }
+				ambient_data = value;
+			}
+		}
 
 		// ディフューズデータ
 		public S_RGBA_Data mp_diffuse_data
 		{
-            get
+			// ゲッタ
+			get
 			{
-                return diffuse_data;
-            }
-            set
+				return diffuse_data;
+			}
+
+			// セッタ
+			set
 			{
-                diffuse_data = value;
-            }
-        }
+				diffuse_data = value;
+			}
+		}
 
 		// エミッションデータ
 		public S_RGBA_Data mp_emission_data
 		{
-            get
+			// ゲッタ
+			get
 			{
-                return emission_data;
-            }
-            set
+				return emission_data;
+			}
+
+			// セッタ
+			set
 			{
-                emission_data = value;
-            }
-        }
+				emission_data = value;
+			}
+		}
 
 		// リフレクションデータ
 		public S_RGBA_Data mp_reflection_data
 		{
-            get
+			// ゲッタ
+			get
 			{
-                return reflection_data;
-            }
-            set
+				return reflection_data;
+			}
+
+			// セッタ
+			set
 			{
-                reflection_data = value;
-            }
-        }
+				reflection_data = value;
+			}
+		}
 
 		// スペキュラデータ
 		public S_RGBA_Data mp_specular_data
 		{
-            get
+			// ゲッタ
+			get
 			{
-                return specular_data;
-            }
-            set
+				return specular_data;
+			}
+
+			// セッタ
+			set
 			{
-                specular_data = value;
-            }
-        }
+				specular_data = value;
+			}
+		}
 
 		// トランスペアレントデータ
 		public S_RGBA_Data mp_transparent_data
 		{
-            get
+			// ゲッタ
+			get
 			{
-                return transparent_data;
-            }
-            set
+				return transparent_data;
+			}
+
+			// セッタ
+			set
 			{
-                transparent_data = value;
-            }
-        }
+				transparent_data = value;
+			}
+		}
 	}
 
 
@@ -316,10 +360,10 @@ namespace ELMaterial_Generator.Load_System
 			{
 				// ☆ 変数宣言 ☆ //
 				List<E_WRITE_SETTING> new_write_setting_list = new List<E_WRITE_SETTING>();   // 新しい書き込み設定リスト
-                
+				
 
-                // 書き込み設定リストへ移動
-                for (int l_now_blend_number = 0; l_now_blend_number < result_material.mp_blend_sum; l_now_blend_number++)
+				// 書き込み設定リストへ移動
+				for (int l_now_blend_number = 0; l_now_blend_number < result_material.mp_blend_sum; l_now_blend_number++)
 				{
 					// ☆ 変数宣言 ☆ //
 					string write_setting = "";   // 書き込み設定
@@ -416,34 +460,34 @@ namespace ELMaterial_Generator.Load_System
 			// カリングモードを判定してセット
 			switch (read_data_list[now_position.mp_now_line].Substring(now_position.mp_now_column))
 			{
-                // 全面
-                case "ALWAYS":
-                    result_material.mp_cull_mode = E_CULL_MODE.e_CULL_ALWAYS;
-                    break;
+				// 全面
+				case "ALWAYS":
+					result_material.mp_cull_mode = E_CULL_MODE.e_CULL_ALWAYS;
+					break;
 
-                // 前面
-                case "FRONT":
-                    result_material.mp_cull_mode = E_CULL_MODE.e_CULL_FRONT;
-                    break;
+				// 前面
+				case "FRONT":
+					result_material.mp_cull_mode = E_CULL_MODE.e_CULL_FRONT;
+					break;
 
-                // 裏面
-                case "BACK":
-                    result_material.mp_cull_mode = E_CULL_MODE.e_CULL_BACK;
-                    break;
+				// 裏面
+				case "BACK":
+					result_material.mp_cull_mode = E_CULL_MODE.e_CULL_BACK;
+					break;
 
-                // その他
-                default:
-                    result_material.mp_cull_mode = E_CULL_MODE.e_CULL_ALWAYS;
-                    break;
-            }
+				// その他
+				default:
+					result_material.mp_cull_mode = E_CULL_MODE.e_CULL_ALWAYS;
+					break;
+			}
 
 
-            // アンチエイリアシングモードへ移動
-            now_position.M_Goto_Next_Data(read_data_list);
-            now_position.M_Goto_Next_Data(read_data_list);
-            now_position.M_Goto_Next_Data(read_data_list);
-            now_position.M_Goto_Next_Data(read_data_list);
-            now_position.M_Goto_Next_Data(read_data_list);
+			// アンチエイリアシングモードへ移動
+			now_position.M_Goto_Next_Data(read_data_list);
+			now_position.M_Goto_Next_Data(read_data_list);
+			now_position.M_Goto_Next_Data(read_data_list);
+			now_position.M_Goto_Next_Data(read_data_list);
+			now_position.M_Goto_Next_Data(read_data_list);
 
 			// アンチエイリアシングモードを判定してセット
 			switch (read_data_list[now_position.mp_now_line].Substring(now_position.mp_now_column))
@@ -451,27 +495,27 @@ namespace ELMaterial_Generator.Load_System
 				// α補間
 				case "ALPHA":
 					result_material.mp_anti_aliasing_mode = E_ANTI_ALIASING_MODE.e_ALPHA;
-                    break;
+					break;
 
 				// 色補間
 				case "PIXEL":
 					result_material.mp_anti_aliasing_mode = E_ANTI_ALIASING_MODE.e_COLOR;
-                    break;
+					break;
 
 				// 線形補間
 				case "LINE":
 					result_material.mp_anti_aliasing_mode = E_ANTI_ALIASING_MODE.e_LINE;
-                    break;
+					break;
 
 				// その他
 				default:
-                    result_material.mp_anti_aliasing_mode = E_ANTI_ALIASING_MODE.e_ALPHA;
-                    break;
+					result_material.mp_anti_aliasing_mode = E_ANTI_ALIASING_MODE.e_ALPHA;
+					break;
 			}
 
 
-            // サンプリングレベルへ移動
-            now_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(read_data_list, "SAMPLING:", now_position);
+			// サンプリングレベルへ移動
+			now_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(read_data_list, "SAMPLING:", now_position);
 			now_position.M_Goto_Next_Data(read_data_list);
 
 			// サンプリングレベルをセット
@@ -500,35 +544,108 @@ namespace ELMaterial_Generator.Load_System
 
 			result_material.mp_transparent_data = M_Convert_String_To_RGBA(read_data_list[now_position.mp_now_line]);
 
-            return result_material;
+			return result_material;
 		}
 
 
-		// 文字列をRGBAデータに変換する　引数：文字列　戻り値：RGBAデータ
+		// 質感情報をロードしてセットする　引数：ファイルパス（相対）　戻り値：マテリアル情報
+		static public S_Mesh_Material_Data M_Load_Material_Inform_And_Set(string in_file_path, ref S_Load_Material_Inform out_material)
+		{
+			// ☆ 変数宣言 ☆ //
+			List<string> read_data_list = new List<string>();   // 読み取ったデータのリスト
+
+			S_Now_File_Data_Position now_position = new S_Now_File_Data_Position(0, 0);	// 現在のファイル操作中の場所
+
+			S_Mesh_Material_Data result_mesh_material = new S_Mesh_Material_Data();	// 結果のマテリアル情報
+
+
+			// ファイルのデータを読み取る
+			{
+				// ☆　変数宣言 ☆ //
+				StreamReader file_data = new StreamReader(in_file_path); // 指定されたファイルのデータ
+
+				bool is_file_end = false;   // ファイルの終わりに来たかどうか
+
+
+				// ファイルの終わりに来るまで読み取る
+				while (is_file_end == false)
+				{
+					// ☆ 変数宣言 ☆ //
+					string new_line = file_data.ReadLine();  // 新しい行のデータ
+
+
+					// もうデータがないなら終了
+					if (new_line == null)
+					{
+						is_file_end = true;
+					}
+
+					// データがあるなら読み取ったデータのリストに追加
+					else
+					{
+						read_data_list.Add(new_line);
+					}
+				}
+
+				// ファイルを閉じる
+				file_data.Close();
+			}
+
+
+            // アンビエントをロード
+            now_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(read_data_list, "AMBIENT:", now_position);
+			result_mesh_material.mp_ambient_data = M_Convert_String_To_RGBA(read_data_list[now_position.mp_now_line].Substring(now_position.mp_now_column));
+
+			// ディフューズをロード
+			now_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(read_data_list, "DIFFUSE:", now_position);
+			result_mesh_material.mp_diffuse_data = M_Convert_String_To_RGBA(read_data_list[now_position.mp_now_line].Substring(now_position.mp_now_column));
+
+			// エミッションをロード
+			now_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(read_data_list, "EMISSION:", now_position);
+			result_mesh_material.mp_emission_data = M_Convert_String_To_RGBA(read_data_list[now_position.mp_now_line].Substring(now_position.mp_now_column));
+
+			// リフレクションをロード
+			now_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(read_data_list, "REFLECTION:", now_position);
+			result_mesh_material.mp_reflection_data = M_Convert_String_To_RGBA(read_data_list[now_position.mp_now_line].Substring(now_position.mp_now_column));
+
+			// スペキュラをロード
+			now_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(read_data_list, "SPECULAR:", now_position);
+			result_mesh_material.mp_specular_data = M_Convert_String_To_RGBA(read_data_list[now_position.mp_now_line].Substring(now_position.mp_now_column));
+
+			// トランスペアレントをロード
+			now_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(read_data_list, "TRANSPARENT:", now_position);
+			result_mesh_material.mp_transparent_data = M_Convert_String_To_RGBA(read_data_list[now_position.mp_now_line].Substring(now_position.mp_now_column));
+
+            return result_mesh_material;
+		}
+
+
+
+		// 文字列をRGBAデータに変換する　引数：RGBAを示す文字列　戻り値：RGBAデータ
 		static public S_RGBA_Data M_Convert_String_To_RGBA(string in_rgba_string)
 		{
-            // ☆ 変数宣言 ☆ //
-            S_RGBA_Data result_rgba = new S_RGBA_Data();   // 結果のRGBAデータ
+			// ☆ 変数宣言 ☆ //
+			S_RGBA_Data result_rgba = new S_RGBA_Data();   // 結果のRGBAデータ
 
 			int now_word_position = 0;   // 現在の文字位置
 
 
 			// RGBAデータをセット
 			result_rgba.mp_red = float.Parse(CS_My_Math_System.M_Get_String_Until_This_Word(now_word_position, in_rgba_string, ','));
-            now_word_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(in_rgba_string, ",", now_word_position);
+			now_word_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(in_rgba_string, ",", now_word_position);
 
 			result_rgba.mp_green = float.Parse(CS_My_Math_System.M_Get_String_Until_This_Word(now_word_position, in_rgba_string, ','));
-            now_word_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(in_rgba_string, ",", now_word_position);
+			now_word_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(in_rgba_string, ",", now_word_position);
 
 			result_rgba.mp_blue = float.Parse(CS_My_Math_System.M_Get_String_Until_This_Word(now_word_position, in_rgba_string, ','));
-            now_word_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(in_rgba_string, ",", now_word_position);
+			now_word_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(in_rgba_string, ",", now_word_position);
 
 			result_rgba.mp_alpha = float.Parse(CS_My_Math_System.M_Get_String_Until_This_Word(now_word_position, in_rgba_string, ','));
-            now_word_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(in_rgba_string, ",", now_word_position);
+			now_word_position = CS_My_Math_System.M_Search_Word_And_Go_To_Right(in_rgba_string, ",", now_word_position);
 
 
 			return result_rgba;
-        }
+		}
 
 	}
 }
